687ebf31e9025231be4c88950b8791e1ca1a4d9d
gegehprast <gegeh.prast@gmail.com>
2021-07-06 10:32:18 +0700

    remove onInstalled

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-let color = '#3aa757';
-
 const clickHandler = function(e) {
     console.log(e);
     let url = e.pageUrl;
@@ -15,12 +13,6 @@ const clickHandler = function(e) {
 const createContextMenusCallback = function(e) {
     console.log(e);
 }
-
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
 
 const bypass = chrome.contextMenus.create({
     'id': 'bypass_shortlink',
