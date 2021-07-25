7ddd6e54036372548a6cc522e2404ef57fd3b5e1
gegehprast <gegeh.prast@gmail.com>
2021-07-25 13:28:24 +0700

    revert back to using contextMenus

--- mv2/background.js
+++ mv2/background.js
@@ -1,83 +1,23 @@
-const SHALLTY_API = 'https://shallty.kyuun.id/';
 const SHALLTY_WEB = 'https://shallty.moe/';
 
-/**
- * Update available parsers on storage.
- * 
- * @param {string} parsers 
- */
-const setParsers = (parsers) => {
-    chrome.storage.sync.set({
-        parsers: parsers
-    });
-}
-
-/**
- * Get available parsers from storage.
- * 
- * @returns {Promise<string[]>}
- */
-const getParsers = () => {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get('parsers', function(result) {
-            if (chrome.runtime.lastError) {
-                return reject(chrome.runtime.lastError);
-            }
-
-            resolve(result.parsers)
-        });
-    })
-}
-
-const onStartup = async () => {
-    const parsers = await fetch(SHALLTY_API + 'api/parsers').then(res => res.json());
-
-    if (parsers.data) {
-        setParsers(parsers.data);
-    }
-}
-
-/**
- * Check if a parser is availbale for an url.
- * 
- * @param {string} url 
- * @returns 
- */
-const shouldParseUrl = async (url) => {
-    const parsers = await getParsers();
-
-    let parserExists = false;
+const clickHandler = function(e) {
+    let url = e.pageUrl;
     
-    for (let i = 0; i < parsers.length; i++) {
-        const parser = parsers[i];
-        
-        if (url.includes(parser)) {
-            parserExists = true;
-            break;
-        }
+    if (e.linkUrl) {
+        url = e.linkUrl;
     }
-
-    return parserExists
-}
-
-/**
- * 
- * @param {number} tabId 
- * @param {chrome.tabs.TabChangeInfo} changeInfo 
- * @param {chrome.tabs.Tab} tab 
- */
-const onTabUpdated = async (tabId, changeInfo, tab) => {
-    const shouldParse = await shouldParseUrl(tab.url)
     
-    if (shouldParse && !tab.url.includes(SHALLTY_API) && !tab.url.includes(SHALLTY_WEB)) {
-        chrome.tabs.update(tabId, {
-            url: `${SHALLTY_WEB}?shortlink=${tab.url}&open_in_new_tab=false`
-        })
-    }
+    chrome.tabs.create({'url' : `${SHALLTY_WEB}?shortlink=${url}` });
 }
 
-chrome.tabs.onUpdated.addListener(onTabUpdated);
+const createContextMenusCallback = function(e) {
+    console.log(e);
+}
 
-chrome.runtime.onInstalled.addListener(onStartup);
+const bypass = chrome.contextMenus.create({
+    'id': 'bypass_shortlink',
+    'title': 'Bypass Shortlink',
+    'contexts': ['link'],
+}, createContextMenusCallback );
 
-chrome.runtime.onStartup.addListener(onStartup);
+chrome.contextMenus.onClicked.addListener(clickHandler);
